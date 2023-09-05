80c185ed11d1aeb86041dc5324d8df3207e59319
sineokayaa <a.sineokaya@g.nsu.ru>
2023-09-05 12:02:17 +0700

    00003

--- main.py
+++ main.py
@@ -3,5 +3,18 @@ Group:
 Varfolomeeva Viktoria
 Sineokaya Anastasia
 """
+import RU_LOCAL as RU
+text = input(RU.intro)
+words = 0
 
-text = input('')
+text = text.split(' ')
+print(text)
+print(len(text))
+
+for i in range(len(text)):
+    print(text[i])
+    if text[i] != '':
+        words += 1
+        text.pop(i)
+print(words)
+print(text)
