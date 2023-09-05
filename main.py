1887605356310ea7bfc2c6538acd1dcb8be8ce4b
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-09-05 12:10:51 +0700

    12

--- main.py
+++ main.py
@@ -8,13 +8,7 @@ text = input(RU.intro)
 words = 0
 
 text = text.split(' ')
-print(text)
-print(len(text))
 
 for i in range(len(text)):
-    print(text[i])
     if text[i] != '':
         words += 1
-        text.pop(i)
-print(words)
-print(text)
