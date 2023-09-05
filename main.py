f9bff4675c810111a5e0c5f551439f01882d2ea4
sineokayaa <a.sineokaya@g.nsu.ru>
2023-09-05 12:22:47 +0700

    00006

--- main.py
+++ main.py
@@ -13,13 +13,6 @@ text = text.split(' ')
 print(text)
 print(len(text))
 
-for i in range(len(text)):
-    print(text[i])
-    if text[i] != '':
-        words += 1
-        text.pop(i)
-print(words)
-print(text)
 
 for i in range(len(text)):
     if text[i] != '':
