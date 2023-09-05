932eff153c7ce01aa99d0a63788ec16f7f9f42a6
sineokayaa <a.sineokaya@g.nsu.ru>
2023-09-05 12:19:20 +0700

    00004

---
+++ kkj.py
@@ -0,0 +1,28 @@
+"""
+Group:
+Varfolomeeva Viktoria
+Sineokaya Anastasia
+"""
+import RU_LOCAL as RU
+
+text = input(RU.intro)
+text_letters = text
+words = 0
+
+text = text.split(' ')
+print(text)
+print(len(text))
+
+for i in range(len(text)):
+    if text[i] != '':
+        words += 1
+print(words)
+
+vowels = ['a', 'y', 'i', 'o', ' e']
+count_w = 0
+
+for i in range(len(text_letters)):
+    if text_letters[i] in vowels:
+        count_w += 1
+print(count_w)
+
