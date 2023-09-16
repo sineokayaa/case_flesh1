ed2d09445a7eb3b7583b34b6db4c2789301f4491
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2023-09-16 13:38:49 +0700

    fin12

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import RU_LOCAL as RU
 from textblob import TextBlob
 from deep_translator import GoogleTranslator
 
-text = input(RU.intro)
+text = input(RU.INTRO)
 text_letters = text
-text_orig = TextBlob(text)
+text_orig = text
 lang = ''
 
 text = text.split(' ')
@@ -34,23 +34,24 @@ else:
     FRE = 206.835 - 1.015 * ASL - 84.6 * ASW
 
 if FRE > 80:
-    result = RU.FRE_junior
+    result = RU.FRE_JUNIOR
 elif FRE > 50:
-    result = RU.FRE_pupils
+    result = RU.FRE_PUPILS
 elif FRE > 25:
-    result = RU.FRE_students
+    result = RU.FRE_STUDENTS
 else:
-    result = RU.FRE_graduate
+    result = RU.FRE_GRADUATE
 
 translated = GoogleTranslator(source='auto', target='en').translate(text_orig)
+translated = TextBlob(translated)
 
 pol = translated.sentiment.polarity
 if pol <= -1/3:
-    pol_ans = RU.pol_negative
+    pol_ans = RU.POL_NEGATIVE
 elif pol <= 1/3:
-    pol_ans = RU.pol_neutral
+    pol_ans = RU.POL_NEUTRAL
 else:
-    pol_ans = RU.pol_positive
+    pol_ans = RU.POL_POSITIVE
 
 sub = str(translated.sentiment.subjectivity * 100) + '%'
 
@@ -60,5 +61,5 @@ print(RU.AVERAGE_S_L, ASL)
 print(RU.AVERAGE_S_W, ASW)
 print(RU.FRE, FRE)
 print(result)
-print(RU.pol, pol_ans)
-print(RU.sub, sub)
+print(RU.POL, pol_ans)
+print(RU.SUB, sub)
